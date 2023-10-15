4c1eaf6d21882939d9113105ee9f099cee9107e2
Josue <joshvlbn@gmail.com>
2023-10-15 18:16:12 -0300

    estilos dispositivos moviles terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const navToggleBtn = document.querySelector('.navbar__toggle');
 const navMenu = document.querySelector('.navbar__menu');
+const navLinks = document.querySelectorAll('.navbar__menu__link');
 
 navToggleBtn.addEventListener('click', () => {
 
@@ -11,3 +12,23 @@ navToggleBtn.addEventListener('click', () => {
         navToggleBtn.setAttribute('aria-label', 'Abrir menu')
     }
 })
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        navLinks.forEach((item) => {
+            item.classList.remove('navbar__menu__link--active');
+        })
+        link.classList.add('navbar__menu__link--active');
+
+        navMenu.classList.remove('navbar__menu--visible');
+        navToggleBtn.setAttribute('aria-label', 'Abrir menu')
+    });
+});
+
+/* document.addEventListener('click', (event) => {
+    if (!navMenu.contains(event.target) && event.target !== navToggleBtn) {
+        navMenu.classList.remove('navbar__menu--visible');
+        navToggleBtn.setAttribute('aria-label', 'Abrir menu')
+    }
+    console.log('hola')
+}); */
