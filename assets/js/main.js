9ec0f28d66eab54e3b328adaf7479e9849546f45
Josue <joshvlbn@gmail.com>
2023-10-15 22:11:47 -0300

    avance en proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
 const navToggleBtn = document.querySelector('.navbar__toggle');
 const navMenu = document.querySelector('.navbar__menu');
 const navLinks = document.querySelectorAll('.navbar__menu__link');
+const navToggleBtnIconOpen = document.querySelector('.fa-bars');
+const navToggleBtnIconClose = document.querySelector('.fa-xmark');
 
 navToggleBtn.addEventListener('click', () => {
 
     navMenu.classList.toggle('navbar__menu--visible')
+    navToggleBtnIconOpen.classList.toggle('navbar__icons--novisible')
+    navToggleBtnIconClose.classList.toggle('navbar__icons--novisible')
 
     if (navMenu.classList.contains('navbar__menu--visible')) {
         navToggleBtn.setAttribute('aria-label', 'Cerrar menu');
+
     } else {
         navToggleBtn.setAttribute('aria-label', 'Abrir menu')
     }
